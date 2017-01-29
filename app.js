fdfc3a3731d0b0754ed6585298691facd5da9434
phil n <surkins@gmail.com>
2017-01-29 14:20:23 -0800

    Add basic app.js index.html and main.css

---
+++ app.js
@@ -0,0 +1,65 @@
+
+var list = (function () {
+  var list = [];
+
+  
+
+}) ();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// var list = (function () {
+//   var list = [];
+//
+//   //DOM
+//   var buttonEl = document.getElementById("add-button");
+//   var inputEl = document.getElementById("input-field");
+//   var todoEl = document.getElementById('to-do');
+//
+//   //bind events
+//
+//   // buttonEl.onclick = function () {
+//   //   document.write('asdfjkljkl;!');
+//   // };
+//   //
+//
+//   buttonEl.onclick = addItem () ;
+//
+//   function addItem () {
+//     // inputEl.textContent(item);
+//     var inputValue = inputEl.value();
+//     list.push(inputValue);
+//   }
+//
+//   function arrResult () {
+//     console.log('array', list);
+//   }
+//
+//   return {
+//     addItem: addItem,
+//     arrResult: arrResult
+//   };
+//
+// }) ();
