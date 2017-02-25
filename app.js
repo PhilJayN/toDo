9e554e4966eb11459eb5d3030c86d5298a3b3564
Philip Nguyen <phi.nguyen@laverne.edu>
2017-02-24 16:49:25 -0800

    Test

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 
 var list = (function () {
-  // var list = [];
-
-console.log('asdfjk');
     //DOM
       var $inputFieldEl = $("#input-field");
       var $buttonEl = $("#add-button");
       var $todoEl = $("#to-do");
+      var $listItemEl;
+      var $delBtnEl;
 
       //bind events
       $buttonEl.on("click", addItem);
@@ -15,22 +14,79 @@ console.log('asdfjk');
         console.log ('clicked!');
         var inputValue = $inputFieldEl.val();
         //creates li, then puts user input value inside.
-        var $listItem = $("<li>").text(inputValue);
+        $listItemEl = $("<li>");
+        $listItemEl.text(inputValue);
         //creates an x button to use as delete btn
-        var $delBtn = $("<span>").text("x");
-        //append to DOM, or else dangling
-        $listItem.append($delBtn);
-        $todoEl.append($listItem);
+        $delBtnEl = $("<span>");
+        $delBtnEl.addClass("x-btn").text("x");
+        //append to DOM
+        $listItemEl.append($delBtnEl);
+        $todoEl.append($listItemEl);
+
+        // return {
+        //   $listItemEl: $listItemEl,
+        //   $delBtnEl: $delBtnEl
+        // };
       }
+
+      function delItem () {
+        console.log('clikced del');
+      }
+
+      $listItemEl.on("click", delItem);
+
+      // var $spanEl = $(".x-btn");
+      // $spanEl.on("click", function () {
+      //   console.log('23j4kljkl;sd');
+      // });
+
 
       return {
         addItem: addItem,
       };
-
 }) ();
 
 
+console.log('additem', list.addItem);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var $spanEl = $(".x-btn");
+// $spanEl.on("click", function() {
+//   console.log('del!');
+// });
+
+
+      //
+      // function addItem () {
+      //   console.log ('clicked!');
+      //   var inputValue = $inputFieldEl.val();
+      //   //creates li, then puts user input value inside.
+      //   var $listItem = $("<li>").text(inputValue);
+      //   //creates an x button to use as delete btn
+      //   var $delBtn = $("<span>").addClass("x-btn").text("x");
+      //   //append to DOM
+      //   $listItem.append($delBtn);
+      //   $todoEl.append($listItem);
+      //
+      //   return {
+      //     $listItem: $listItem,
+      //     $delBtn: $delBtn
+      //   };
+      //
+      //
+      // }
 
 // function arrResult () {
 //   console.log('array', list);
